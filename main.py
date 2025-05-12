97ca9ad0814d4907ab5f05f6e0868578760caaed
Wookachakaman <eadhu.dhanesh2013@gmail.com>
2025-05-13 07:30:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ def login():
     if request.method == 'POST':
         password = request.form.get('password')
         if password == "wookachakahp":
-            return render_template('website.html')
-        return "Access denied"
-    
+            return redirect('/home') 
+        else:
+            return '''
+                <p style="color:red;">Access Denied. Try again.</p>
+                <form method="POST">
+                    <input type="password" name="password" placeholder="Enter password" required>
+                    <button type="submit">Submit</button>
+                </form>
+            '''
     return '''
         <form method="POST">
-            <input type="password" name="password" placeholder="Enter password">
+            <input type="password" name="password" placeholder="Enter password" required>
             <button type="submit">Submit</button>
         </form>
     '''
+
+@app.route('/home')
+def home():
+    return render_template('website.html')  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
